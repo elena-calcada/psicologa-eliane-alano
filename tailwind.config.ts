17342757643c21d0e2d038da08126a3f8d85c5b1
Elena Calcada Evangelista <calcada.elena@gmail.com>
2024-07-18 11:12:42 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,41 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'xs': { max: "639px" },
+      sm: '640px',
+      'sm-max': { max: "767px" },
+      'sm-lg': { min: '640px', max: '1023px' },
+      md: '768px',
+      'md-max': { max: '1023px' },
+      lg: '1024px',
+      'lg-max': { max: '1279px' },
+      'lg-xl': { min: '1024px', max: '1280px' },
+      xl: '1280px',
+      '2xl': '1440px'
+    },
+    fontFamily: {
+      dm_sans: 'var(--font-dm_sans)',
+      poppins: 'var(--font-poppins)'
+    },
+    backgroundImage: {
+      "home-desktop": 'url(/assets/home.png)',
+      dividertOne: "linear-gradient(90deg, #10496F 0%, rgba(72, 123, 114, 0) 100%)",
+      dividerTwo: "linear-gradient(270deg, #10496F 0%, rgba(72, 123, 114, 0) 100%)"
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          DEFAULT: '#10496F',
+        },
+        blueColor: {
+          backgroundSection: "#F3F7FA",
+          backgroundFooter: "#D8E0E5",
+          buttonColor: "#009BFF",
+          mandalaColor: '#B3DEFB',
+        },
+        titleColor: "#02243A",
+        fontColor: "#525A58",
       },
     },
   },
